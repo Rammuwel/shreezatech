2380456679190af31c48320e3c36fefc1f8d34b2
ram muwel <shrirammuwel02017@gmail.com>
2026-08-06 05:39:31 +0530

    Remove unused axios dependency and dead wave.svg asset

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,8 +1,3 @@
-import axios from 'axios';
-
-window.axios = axios;
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
 const applyTheme = () => {
   const dark = localStorage.getItem('theme') !== 'light';
   document.documentElement.classList.toggle('dark', dark);
